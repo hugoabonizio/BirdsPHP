5e8ff9fb71297141f5c14fcd76a60d20fdae761e
Hugo Abonizio <hugo_abonizio@hotmail.com>
2015-09-30 01:37:02 +0000

    Show error when layout file is missing and accept this to be empty

--- src/renderer.php
+++ src/renderer.php
@@ -8,11 +8,17 @@ class Renderer {
 			ob_start();
 			include 'app/views/' . $controller . '/' . $action . '.php';
 			$yield = ob_get_clean();
-			if (file_exists('app/views/layouts/' . $instance->_layout . '.php')) {
-				include 'app/views/layouts/' . $instance->_layout . '.php';
-			}
+      if (strlen($instance->_layout)) { // layout file setted
+        if (file_exists('app/views/layouts/' . $instance->_layout . '.php')) {
+          include 'app/views/layouts/' . $instance->_layout . '.php';
+        } else {
+          echo "ERROR: layout not found";
+        }
+      } else { // without layout file
+        echo $yield;
+      }
 		} else {
-			echo 'vish';
+			echo "ERROR: view not found";
 		}
 	}	
 }
