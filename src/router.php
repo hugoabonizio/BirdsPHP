174048def09586e2f6f2ce8aa592cb9444d8e333
Hugo Abonizio <hugo_abonizio@hotmail.com>
2015-10-01 20:35:16 +0000

    Implemented params through URI (like /show/:id)

--- src/router.php
+++ src/router.php
@@ -10,13 +10,13 @@ class Router {
       if ($route[1][0] == '/') {
         $route[1] = substr($route[1], 1);
       }
-      $regex_route = '/^' . $route[1] . '$/';
+      $regex_route = '/^' . self::convert_params(str_replace('/', '\/', $route[1])) . '$/';
 			self::$_routes[$regex_route][strtoupper($route[0])] = $route[2];
 		}
 	}
 	
 	static function route($method, $uri) {
-    $uri = substr($uri, 1);
+    $uri = substr($uri, 1); // after first / ("/about", for example)
     foreach (array_keys(self::$_routes) as $route) {
       if (preg_match($route, $uri)) {
 			  return self::$_routes[$route][strtoupper($method)];
@@ -24,4 +24,8 @@ class Router {
     }
 		return 404;
 	}
+  
+  static private function convert_params($string) {
+    return preg_replace("/:(.*)/", "(?<aaa>[a-z-A-B0-9]+)", $string);
+  }
 }
--- tests/framework/router_test.php
+++ tests/framework/router_test.php
@@ -3,6 +3,7 @@ class RouterTest extends \PHPUnit_Framework_TestCase {
 	function setUp() {
 		\Framework\Router::draw(array(
 			['GET', '/', 'welcome#index'],
+      ['GET', '/new', 'welcome#new'],
       ['GET', '/show/:id', 'resource#show']
 		));
 	}
@@ -10,10 +11,17 @@ class RouterTest extends \PHPUnit_Framework_TestCase {
 	function testAddRoute() {
 		$this->assertEquals('welcome#index', \Framework\Router::route('get', '/'));
 		$this->assertEquals('welcome#index', \Framework\Router::route('GET', '/'));
+    $this->assertEquals('welcome#new', \Framework\Router::route('GET', '/new'));
 	}
 	
 	function test404() {
 		$this->assertEquals(404, \Framework\Router::route('GET', '/aaaa'));
     $this->assertEquals(404, \Framework\Router::route('GET', '/a/b'));
 	}
+  
+  function testParams() {
+    $this->assertEquals('resource#show', \Framework\Router::route('GET', '/show/2'));
+    $this->assertEquals('resource#show', \Framework\Router::route('GET', '/show/10'));
+    $this->assertEquals(404, \Framework\Router::route('GET', '/show'));
+  }
 }
