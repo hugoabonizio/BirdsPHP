374a2f3b033ba3f552696b44c9aeec8d7a7132e9
Hugo Abonizio <hugo_abonizio@hotmail.com>
2015-10-05 23:27:01 +0000

    Test changing intentation

--- src/initializer.php
+++ src/initializer.php
@@ -1,60 +1,61 @@
 <?php
 namespace Framework;
 
-if (!isset($_SESSION)) { 
-  session_start(); 
+if (!isset($_SESSION)) {
+  session_start();
 }
+
 include_once 'application.php';
 
 header('Content-Type: text/html; charset=utf-8');
 
 class Initializer extends Application {
-	function __construct() {
-		$this->loadFramework();
-		$this->loadControllers();
+  function __construct() {
+    $this->loadFramework();
+    $this->loadControllers();
     $this->loadModels();
-		$this->loadRoutes();
-	}
-	
-	function loadFramework() {
-		$files = scandir(dirname(__FILE__));
-		foreach($files as $file) {
-			if ($file != '.' and $file != '..') {
-				include_once dirname(__FILE__) .'/' . $file;
-			}
-		}
-	}
-	
-  function loadModels() {
-		if (is_dir('app/models')) {
-			$files = scandir('app/models');
-			foreach($files as $file) {
-				if ($file != '.' and $file != '..') {
-					include 'app/models/' . $file;
-				}
-			}
-		}
-	}
+    $this->loadRoutes();
+  }
   
-	function loadControllers() {
-		if (is_dir('app/controllers')) {
-			$files = scandir('app/controllers');
-			foreach($files as $file) {
-				if ($file != '.' and $file != '..') {
-					include 'app/controllers/' . $file;
-				}
-			}
-		}
-	}
-	
-	function loadRoutes() {
-		if (file_exists('config/routes.php')) {
-			include 'config/routes.php';
-		}
-	}
-	
-	function run() {
-    $path = strtok($_SERVER['REQUEST_URI'],'?');
-		parent::route($_SERVER['REQUEST_METHOD'], $path);
-	}
+  function loadFramework() {
+    $files = scandir(dirname(__FILE__));
+    foreach ($files as $file) {
+      if ($file != '.' and $file != '..') {
+        include_once dirname(__FILE__) . '/' . $file;
+      }
+    }
+  }
+  
+  function loadModels() {
+    if (is_dir('app/models')) {
+      $files = scandir('app/models');
+      foreach ($files as $file) {
+        if ($file != '.' and $file != '..') {
+          include 'app/models/' . $file;
+        }
+      }
+    }
+  }
+  
+  function loadControllers() {
+    if (is_dir('app/controllers')) {
+      $files = scandir('app/controllers');
+      foreach ($files as $file) {
+        if ($file != '.' and $file != '..') {
+          include 'app/controllers/' . $file;
+        }
+      }
+    }
+  }
+  
+  function loadRoutes() {
+    if (file_exists('config/routes.php')) {
+      include 'config/routes.php';
+    }
+  }
+  
+  function run() {
+    $path = strtok($_SERVER['REQUEST_URI'], '?');
+    parent::route($_SERVER['REQUEST_METHOD'], $path);
+  }
 }
